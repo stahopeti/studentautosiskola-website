b1b77630458e7e7ac73fb817bcfb0d27195fb60c
Peter Bence Stahorszki <stahopeti@gmail.com>
2019-09-24 14:46:44 +0200

    tabular implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule, NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    NgxExtendedPdfViewerModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
